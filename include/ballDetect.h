23d7973a4355dce8df3ca6208f88ba36f1b438f0
Sanket Sudake <sanketsudake@gmail.com>
2013-12-06 10:47:51 +0530

    Shot no added

--- include/ballDetect.h
+++ include/ballDetect.h
@@ -1,5 +1,6 @@
 #ifndef __BALLDETECT_H_INCLUDED__
 #define __BALLDETECT_H_INCLUDED__
+
 #include<opencv2/imgproc/imgproc.hpp>
 #include<opencv2/highgui/highgui.hpp>
 #include<opencv2/core/core.hpp>
@@ -7,27 +8,28 @@
 
 using namespace cv;
 
-
 class ballDetect{
-private:
-    //max number of objects to be detected in frame
-    static const int MAX_NUM_OBJECTS = 50;
-    //minimum and maximum object area
-    static const int MIN_OBJECT_AREA = 5*5;
-    static const int MAX_OBJECT_AREA = 20 * 20;
-    //FRAME_HEIGHT*FRAME_WIDTH/1.5;
-    //default capture width and height
-    static const int FRAME_WIDTH = 640;
-    static const int FRAME_HEIGHT = 480;
-    Scalar *minval,*maxval;
-    
-public:
-    ballDetect();
-    string intToString(int number);
-    void drawObject(int x, int y,Mat &frame,int ballIndex,int redIndex);
-    void morphOps(Mat &processed);
-    void trackFilteredObject(int &, int &, Mat , Mat &,int);
-    void initDetect(char *);
+    private:
+        // int shot=0;
+        // max number of objects to be detected in frame
+        static const int MAX_NUM_OBJECTS = 50;
+        //minimum and maximum object area
+        static const int MIN_OBJECT_AREA = 5*5;
+        static const int MAX_OBJECT_AREA = 20 * 20;
+        //FRAME_HEIGHT*FRAME_WIDTH/1.5;
+        //default capture width and height
+        static const int FRAME_WIDTH = 640;
+        static const int FRAME_HEIGHT = 480;
+        Scalar *minval,*maxval;
+
+    public:
+        ballDetect();
+        string intToString(int number);
+        void drawObject(int x, int y,Mat &frame,int ballIndex,int redIndex);
+        void morphOps(Mat &processed);
+        void trackFilteredObject(int &, int &, Mat , Mat &,int);
+        void initDetect(char *);
+        void detectShot();
 };
 
 #endif
