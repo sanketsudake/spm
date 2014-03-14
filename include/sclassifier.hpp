7e17fb47b50dcfa21bc64ab0d3ca62521a82527f
Sanket Sudake <sanketsudake@gmail.com>
2014-03-14 13:35:35 +0530

    Added shot types in sclassifier

--- include/sclassifier.hpp
+++ include/sclassifier.hpp
@@ -10,16 +10,40 @@
 using namespace std;
 using namespace cv;
 
+enum shot_type{
+    //! Template
+    // SPIN TYPE + White final on which side of tangent
+
+    NO_SPIN_HEADON,
+    CUT_SHOT,
+
+    FORWARD_SPIN_HEADON,
+    FORWARD_SPIN_LEFT,
+    FOWARD_SPIN_RIGHT,
+
+    BACKWARD_SPIN_HEADON,
+    BACKWARD_SPIN_LEFT,
+    BACKWARD_SPIN_RIGHT,
+
+    LEFT_SPIN_HEADON,
+    LEFT_SPIN_LEFT,
+    LEFT_SPIN_RIGHT,
+
+    RIGHT_SPIN_HEADON,
+    RIGHT_SPIN_LEFT,
+    RIGHT_SPIN_RIGHT
+};
+
 class Sclassfier
 {
-    private:
-       int alpha, beta;
-       Point contact;
-
-    public:
-       Sclassfier();
-       ~Sclassfier();
-       int classfier(int alpha, int beta, Point contact);
+private:
+    int alpha, beta;
+    Point contact;
+    shot_type shot_class;
+public:
+    Sclassfier();
+    ~Sclassfier();
+    int classfier(int beta, Point contact, int alpha=0);
 };
 
 #endif
--- src/sclassifier.cpp
+++ src/sclassifier.cpp
@@ -11,4 +11,22 @@ Sclassifier:: Sclassifier()
 
 Sclassifier:: ~Sclassifier()
 {
+
+}
+
+int Sclassifier::classfier(int beta, Point contact, int alpha=0)
+{
+    /*! alpha =>
+     * angle between white ball segment before collision
+     * & segment of white ball after collision
+     * beta =>
+     * angle between normal to point of contact  and white final
+     * point of contact =>
+     * point of collision between white ball and target ball
+     * point should be precise considering intersection between
+     * circumference of two circles
+     */
+
+
+    return 1;
 }
